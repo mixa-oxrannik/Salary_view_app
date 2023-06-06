4f5d3d3053fb94809bf04d8f875b9551795568e8
kostya <mixa_oxrannik3@mail.ru>
2023-06-06 17:01:57 +0700

    Added a function to check the token and a method for issuing salary data.

--- routers.py
+++ routers.py
@@ -1,9 +1,10 @@
 import jwt
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from fastapi.security import HTTPBasicCredentials
 
 from config import SECRET_KEY
-from services import authenticate_employee, generate_token
+from services import authenticate_employee, generate_token, verify_token
+from database import employees
 
 router = APIRouter()
 
@@ -12,3 +13,12 @@ async def login(credentials: HTTPBasicCredentials):
     username = authenticate_employee(credentials)
     token = generate_token(username)
     return {"access_token": jwt.encode(token, SECRET_KEY, algorithm="HS256")}
+
+
+@router.post("/salary")
+async def get_salary(token: str = Depends(verify_token)):
+    username = token["username"]
+    return {
+        "salary": employees[username]["salary"],
+        "next_raise": employees[username]["next_raise"]
+    }
--- services.py
+++ services.py
@@ -1,9 +1,12 @@
 from datetime import datetime, timedelta
+from typing import Optional
 
-from fastapi import HTTPException
+import jwt
+from fastapi import HTTPException, Depends
 from fastapi.security import HTTPBasicCredentials
 
 from database import employees
+from config import SECRET_KEY
 
 def generate_token(username: str):
     expiration_time = str(datetime.utcnow() + timedelta(minutes=30)) # токен действует 30 минут
@@ -18,3 +21,9 @@ def authenticate_employee(credentials: HTTPBasicCredentials):
     if username not in employees or employees[username]["password"] != password:
         raise HTTPException(status_code=401, detail="Incorrect username or password")
     return username
+
+
+def verify_token(token: Optional[str] = None, username: Optional[str] = Depends(authenticate_employee)):
+    decoded_token = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+
+    return decoded_token
